d28b0c2fb112d6bb31f6354ef968444bb413a4a2
Deepesh Kumar Verma <131790009+DeepeshKumarVerma@users.noreply.github.com>
2025-07-11 00:35:40 +0530

    Delete deployment directory

--- deployment/django/bert_sentiment_project/bert_sentiment_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views # import your view 
-
-urlpatterns= [
-    path('', views.sentiment_view, name= 'home'),  # default route
-    path('predict/', views.sentiment_view, name= 'predict'), # # or use separate prediction route
-]
